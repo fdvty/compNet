663ac86f3d920cfe3a7d99267140190682d58972
Zirui Liu <zirui.liu@pku.edu.cn>
2020-03-11 20:00:34 +0800

    test

---
+++ app/email.py
@@ -0,0 +1,26 @@
+from threading import Thread
+from flask import render_template
+from flask_mail import Message
+from app import app, mail
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(subject, recipients, text_body, html_body):
+    msg = Message(subject=subject, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    Thread(target=send_async_email, args=(app, msg)).start()
+
+
+def send_password_reset_email(user):
+    token = user.get_reset_password_token()
+
+    send_email('[MicroBlog] Reset Your Password',
+               recipients=[user.email],
+               text_body=render_template('email/reset_password.txt', user=user, token=token),
+               html_body=None)
+
