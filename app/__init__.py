737a5c139249fb7dcc797e141f22ebe76000dae5
Zirui Liu <zirui.liu@pku.edu.cn>
2020-04-23 10:16:00 +0800

    fix bug

--- app/__init__.py
+++ app/__init__.py
@@ -47,7 +47,7 @@ if not app.debug:
         mail_handler = SMTPHandler(
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
             fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            toaddrs=app.config['ADMINS'], subject='SmartClinic Failure',
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
@@ -55,7 +55,7 @@ if not app.debug:
     # 出错时打log
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+    file_handler = RotatingFileHandler('logs/smartclinic.log', maxBytes=10240,
                                        backupCount=10)
     file_handler.setFormatter(logging.Formatter(
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
@@ -63,4 +63,4 @@ if not app.debug:
     app.logger.addHandler(file_handler)
 
     app.logger.setLevel(logging.INFO)
-    app.logger.info('Microblog startup')
+    app.logger.info('SmartClinic startup')
--- app/email.py
+++ app/email.py
@@ -19,7 +19,7 @@ def send_email(subject, recipients, text_body, html_body):
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
 
-    send_email('[MicroBlog] Reset Your Password',
+    send_email('[SmartClinic] Reset Your Password',
                recipients=[user.email],
                text_body=render_template('email/reset_password.txt', user=user, token=token),
                html_body=None)
